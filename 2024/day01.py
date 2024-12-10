877d7f445b8640b58b003771bc22a4f1118a5c5b
Onur Arıkan <onur93arikan@gmail.com>
2024-12-10 22:03:46 +0300

    Fix shebangs

--- 2024/day01.py
+++ 2024/day01.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env/python
+#! /usr/bin/env python
 
 from utils import input_text, input_lines
 
--- 2024/day03.py
+++ 2024/day03.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env/python
+#! /usr/bin/env python
 import re
 
 from utils import input_text, input_lines
