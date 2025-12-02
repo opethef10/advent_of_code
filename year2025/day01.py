81a297732b38d91a730523fc76296af963c82e06
Onur Arıkan <onur93arikan@gmail.com>
2025-12-02 18:51:05 +0100

    Add 2025 day1 pt2

--- year2025/day01.py
+++ year2025/day01.py
@@ -18,4 +18,31 @@ def part1(lines, text):
 
 
 def part2(lines, text):
-    return
+    total_zeroes = 0
+    current_position = START_POINT
+    for line in lines:
+        previous_position = current_position
+        direction, value = line[0], int(line[1:])
+        if direction == "L":
+            current_position -= value
+        elif direction == "R":
+            current_position += value
+        crossed, current_position = divmod(current_position, NUM_POSITIONS)
+        abs_crossed = abs(crossed)
+
+        if crossed <= 0:
+            if previous_position != 0 and current_position == 0:
+                total_zeroes += abs_crossed + 1
+
+            elif previous_position != 0 and current_position != 0:
+                total_zeroes += abs_crossed
+
+            elif previous_position == 0 and current_position == 0:
+                total_zeroes += abs_crossed
+
+            elif previous_position == 0 and current_position != 0:
+                total_zeroes += max(0, abs_crossed - 1)
+        elif crossed > 0:
+            total_zeroes += crossed
+
+    return total_zeroes
