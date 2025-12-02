930e6552916d5101f7fbdcb1269e183501de2467
Onur Arıkan <onur93arikan@gmail.com>
2025-12-02 22:54:45 +0100

    Edit template

--- template.py
+++ template.py
@@ -1,6 +1,6 @@
-def part1(lines: list[str], text: str):
-    pass
+def part1(lines: list[str], text: str) -> int:
+    return 0
 
 
-def part2(lines: list[str], text: str):
-    pass
+def part2(lines: list[str], text: str) -> int:
+    return 0
